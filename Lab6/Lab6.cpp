1d007f960f8f7921e857b2428ca35f5b3c216daa
jpeeney <70214218+jpeeney@users.noreply.github.com>
2022-12-16 23:55:10 -0500

    Added explanatory comment
    Ran out of time at the end of the semester

--- Lab6/Lab6.cpp
+++ Lab6/Lab6.cpp
@@ -4,6 +4,16 @@
 //10-27-22
 #include "Engine.h"
 
+/*
+Here is what I would do to finish:
+
+- implement timeHit and lastHit variables like in the arena game 
+so a Goblin or target isn't killed instantly
+- add more Goblin types
+- balance health, damage, and mitigation values
+
+*/
+
 int main()
 {
 	// Declare an instance of Engine
